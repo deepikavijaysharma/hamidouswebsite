99988878316cf7a7b32130cd0e20b0cb37970f10
angansen <angansen@gmail.com>
2018-02-23 17:33:44 +0530

    analytics.js added

---
+++ site/src/js/viewModels/analytics.js
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2014, 2017, Oracle and/or its affiliates.
+ * The Universal Permissive License (UPL), Version 1.0
+ */
+/*
+ * Your dashboard ViewModel code goes here
+ */
+define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojtabs', 'ojs/ojconveyorbelt'],
+ function(oj, ko, $) {
+  
+    function tabviewmodel() {
+      var self = this;
+     
+
+    }
+    return new tabviewmodel();
+  }
+);
