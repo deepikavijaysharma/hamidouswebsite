298a1079a0a99427cdf447681ac5b5594c3a3f67
angansen <angansen@gmail.com>
2018-02-26 20:28:24 +0530

    Repetitive classes shown in courses is fixed

--- site/src/js/viewModels/training.js
+++ site/src/js/viewModels/training.js
@@ -2520,6 +2520,7 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'date', 'ojs/ojknockout', 'ojs/ojtab
                 self.cclass().status([]);
                 self.cclass().key_event(false);
                 resetSchedule();
+                editor_instance_data4="";
                 // $(".classlist").empty();
             }
 
@@ -2737,6 +2738,7 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'date', 'ojs/ojknockout', 'ojs/ojtab
                     schedules: self.cclass().schedules()
                 });
                 $("#addclasstoclass").ojDialog("close");
+                $(".classlist").empty();
                 updateCourseClass();
                 //resetClass();
                 editor_instance_data1 = "";
