604471d451b9bf3e8547715ccc60351e56712f8b
ashrithamalli <ashritha.malli@gmail.com>
2018-05-30 17:51:04 +0530

    infrastructure fix for server

--- site/src/js/viewModels/charts/demos.js
+++ site/src/js/viewModels/charts/demos.js
@@ -271,22 +271,243 @@ self.subchart13 = {
 
 
 			
-			self.fetchdetails = function () {
-			 var result = null;
-    $.ajax({
-        async: false,
-        url: "http://localhost:8000/css/infrastructure.json",
-        dataType: "json",
-        success: function(data){
-            result = data;
-        }
-	});
-    return result;
-			
-			}
-			self.fetchdetails();
-			console.log("all data 2"+self.fetchdetails());
-            self.datasource = new oj.ArrayTableDataSource(self.fetchdetails(), {
+			var envArray = [{
+                    id: '1',
+                    Group: 'Platform',
+                    Team: 'Integration',
+                    TeamLead: 'Craig Mikus',
+                    GSEAccountOwner: '<a href="mailto:bruce.bailey@oracle.com">Bruce Bailey</a>',
+                    GSEReportLink: 'https://dss-obiee.oracleads.com/analytics/saw.dll?Dashboard&PortalPath=%2Fshared%2FGSE%20Public%2F_portal%2FDemo%20Activity%20Report&Page=Environment%20Usage%20%26%20Assignments&PageIdentifier=dci6so08o36rkjab&BookmarkState=c646hqtff9i0v2tp4hfhs3i4p2&opti',
+                    GSEUCMEnvironmentsTotalNumber: 6,
+                    GSEUCMEnvironmentsZeroDemos: 2,
+                    TotalDemos: 8,
+                    FutureDemos: 0,
+                    DemosMonthtoDate: 0,
+                    DemosPriorMonth: 1,
+                    Demos2MonthsBack: 7,
+                    TotalPastDemos: 8,
+                    PercentwithZeroDemos: 33,
+                    DemosEnvMonth: 0.6
+                },
+                {
+                    id: '2',
+                    Group: 'Platform',
+                    Team: 'IaaS/AppDev',
+                    TeamLead: 'Joe Corvaia',
+                    GSEAccountOwner: '<a href="mailto:aravind.tadakamalla@oracle.com;anoop.deshpande@oracle.com">Aravind Tadakamalla, Anoop Deshpande</a>',
+                    GSEReportLink: 'https://dss-obiee.oracleads.com/analytics/saw.dll?Dashboard&PortalPath=%2Fshared%2FGSE%20Public%2F_portal%2FDemo%20Activity%20Report&Page=Environment%20Usage%20%26%20Assignments&PageIdentifier=dci6so08o36rkjab&BookmarkState=vdet2qqng5bs1jqk2trs0gabtm&opti',
+                    GSEUCMEnvironmentsTotalNumber: 5,
+                    GSEUCMEnvironmentsZeroDemos: 2,
+                    TotalDemos: 7,
+                    FutureDemos: 0,
+                    DemosMonthtoDate: 0,
+                    DemosPriorMonth: 6,
+                    Demos2MonthsBack: 1,
+                    TotalPastDemos: 7,
+                    PercentwithZeroDemos: 40,
+                    DemosEnvMonth: 0.7
+                },
+                {
+                    id: '3',
+                    Group: 'Platform',
+                    Team: 'Security',
+                    TeamLead: 'Michael Freel',
+                    GSEAccountOwner: '<a href="mailto:waymon.whiting@oracle.com">Waymon Whiting</a>',
+                    GSEReportLink: 'https://dss-obiee.oracleads.com/analytics/saw.dll?Dashboard&PortalPath=%2Fshared%2FGSE%20Public%2F_portal%2FDemo%20Activity%20Report&Page=Environment%20Usage%20%26%20Assignments&PageIdentifier=dci6so08o36rkjab&BookmarkState=928l12lgdltfkpvr64bvcd1hsu&opti',
+                    GSEUCMEnvironmentsTotalNumber: 8,
+                    GSEUCMEnvironmentsZeroDemos: 5,
+                    TotalDemos: 8,
+                    FutureDemos: 1,
+                    DemosMonthtoDate: 0,
+                    DemosPriorMonth: 3,
+                    Demos2MonthsBack: 4,
+                    TotalPastDemos: 7,
+                    PercentwithZeroDemos: 63,
+                    DemosEnvMonth: 0.4
+                },
+                {
+                    id: '4',
+                    Group: 'Platform',
+                    Team: 'Information Management',
+                    TeamLead: 'Paul Miller',
+                    GSEAccountOwner: '<a href="mailto:david.bayard@oracle.com;catherine.szen@oracle.com">David Bayard, Catherine Szen</a>',
+                    GSEReportLink: 'https://dss-obiee.oracleads.com/analytics/saw.dll?Dashboard&PortalPath=%2Fshared%2FGSE%20Public%2F_portal%2FDemo%20Activity%20Report&Page=Environment%20Usage%20%26%20Assignments&PageIdentifier=dci6so08o36rkjab&BookmarkState=br81cc5tfmpnt7c1gu46cksibu&opti',
+                    GSEUCMEnvironmentsTotalNumber: 5,
+                    GSEUCMEnvironmentsZeroDemos: 0,
+                    TotalDemos: 15,
+                    FutureDemos: 1,
+                    DemosMonthtoDate: 1,
+                    DemosPriorMonth: 8,
+                    Demos2MonthsBack: 5,
+                    TotalPastDemos: 14,
+                    PercentwithZeroDemos: 0,
+                    DemosEnvMonth: 1.3
+                },
+                {
+                    id: '5',
+                    Group: 'Platform',
+                    Team: 'Adrienne Howard',
+                    TeamLead: 'Adam Driver',
+                    GSEAccountOwner: '<a href="mailto:adrienne.howard@oracle.com">Adrienne Howard</a>',
+                    GSEReportLink: 'https://dss-obiee.oracleads.com/analytics/saw.dll?Dashboard&PortalPath=%2Fshared%2FGSE%20Public%2F_portal%2FDemo%20Activity%20Report&Page=Environment%20Usage%20%26%20Assignments&PageIdentifier=dci6so08o36rkjab&BookmarkState=d7sk0f94vcuu7d10e473he8uq6&opti',
+                    GSEUCMEnvironmentsTotalNumber: 6,
+                    GSEUCMEnvironmentsZeroDemos: 0,
+                    TotalDemos: 29,
+                    FutureDemos: 0,
+                    DemosMonthtoDate: 2,
+                    DemosPriorMonth: 18,
+                    Demos2MonthsBack: 9,
+                    TotalPastDemos: 29,
+                    PercentwithZeroDemos: 0,
+                    DemosEnvMonth: 2.2
+                },
+                {
+                    id: '6',
+                    Group: 'OD',
+                    Team: 'OD',
+                    TeamLead: 'Carl Griffin',
+                    GSEAccountOwner: '<a href="mailto:ashish.sohane@oracle.com">Ashish Sohane</a>',
+                    GSEReportLink: 'https://dss-obiee.oracleads.com/analytics/saw.dll?Dashboard&PortalPath=%2Fshared%2FGSE%20Public%2F_portal%2FDemo%20Activity%20Report&Page=Environment%20Usage%20%26%20Assignments&PageIdentifier=dci6so08o36rkjab&BookmarkState=9befjq9dihibgoie49kqqgu6nm&opti',
+                    GSEUCMEnvironmentsTotalNumber: 18,
+                    GSEUCMEnvironmentsZeroDemos: 11,
+                    TotalDemos: 25,
+                    FutureDemos: 6,
+                    DemosMonthtoDate: 6,
+                    DemosPriorMonth: 13,
+                    Demos2MonthsBack: 0,
+                    TotalPastDemos: 19,
+                    PercentwithZeroDemos: 61,
+                    DemosEnvMonth: 0.5
+                },
+                {
+                    id: '7',
+                    Group: 'ECA',
+                    Team: 'National',
+                    TeamLead: 'Edwin Upson',
+                    GSEAccountOwner: '<a href="mailto:william.x.green@oracle.com">William Green</a>',
+                    GSEReportLink: 'https://dss-obiee.oracleads.com/analytics/saw.dll?Dashboard&PortalPath=%2Fshared%2FGSE%20Public%2F_portal%2FDemo%20Activity%20Report&Page=Environment%20Usage%20%26%20Assignments&PageIdentifier=dci6so08o36rkjab&BookmarkState=9dr5n9lrt119sg3b08ustngkji&opti',
+                    GSEUCMEnvironmentsTotalNumber: 11,
+                    GSEUCMEnvironmentsZeroDemos: 1,
+                    TotalDemos: 66,
+                    FutureDemos: 12,
+                    DemosMonthtoDate: 10,
+                    DemosPriorMonth: 37,
+                    Demos2MonthsBack: 7,
+                    TotalPastDemos: 54,
+                    PercentwithZeroDemos: 9,
+                    DemosEnvMonth: 2.3
+                },
+                {
+                    id: '8',
+                    Group: 'ECA',
+                    Team: 'Enterprise Named & EGA/CPG/Util',
+                    TeamLead: 'Robin Link',
+                    GSEAccountOwner: '<a href="mailto:job.miller@oracle.com;jeff.knudsen@oracle.com">Job Miller, Jeff Knudsen</a>',
+                    GSEReportLink: 'https://dss-obiee.oracleads.com/analytics/saw.dll?Dashboard&PortalPath=%2Fshared%2FGSE%20Public%2F_portal%2FDemo%20Activity%20Report&Page=Environment%20Usage%20%26%20Assignments&PageIdentifier=dci6so08o36rkjab&BookmarkState=iaqgabonve569q9u5vomv6bkd6&opti',
+                    GSEUCMEnvironmentsTotalNumber: 5,
+                    GSEUCMEnvironmentsZeroDemos: 2,
+                    TotalDemos: 11,
+                    FutureDemos: 2,
+                    DemosMonthtoDate: 0,
+                    DemosPriorMonth: 8,
+                    Demos2MonthsBack: 1,
+                    TotalPastDemos: 9,
+                    PercentwithZeroDemos: 40,
+                    DemosEnvMonth: 0.8
+                },
+                {
+                    id: '9',
+                    Group: 'ECA',
+                    Team: 'Retail & Healthcare',
+                    TeamLead: 'Alan Levine',
+                    GSEAccountOwner: '<a href="mailto:michael.x.glas@oracle.com">Michael Glas</a>',
+                    GSEReportLink: 'https://dss-obiee.oracleads.com/analytics/saw.dll?Dashboard&PortalPath=%2Fshared%2FGSE%20Public%2F_portal%2FDemo%20Activity%20Report&Page=Environment%20Usage%20%26%20Assignments&PageIdentifier=dci6so08o36rkjab&BookmarkState=66i45isgrfb369sf31bbl98hua&opti',
+                    GSEUCMEnvironmentsTotalNumber: 4,
+                    GSEUCMEnvironmentsZeroDemos: 1,
+                    TotalDemos: 10,
+                    FutureDemos: 0,
+                    DemosMonthtoDate: 0,
+                    DemosPriorMonth: 2,
+                    Demos2MonthsBack: 8,
+                    TotalPastDemos: 10,
+                    PercentwithZeroDemos: 25,
+                    DemosEnvMonth: 1.2
+                },
+                {
+                    id: '10',
+                    Group: 'ECA',
+                    Team: 'FSI/Telco',
+                    TeamLead: 'Larry Welles',
+                    GSEAccountOwner: '<a href="mailto:walter.romanski@oracle.com">Walter Romanski</a>',
+                    GSEReportLink: 'https://dss-obiee.oracleads.com/analytics/saw.dll?Dashboard&PortalPath=%2Fshared%2FGSE%20Public%2F_portal%2FDemo%20Activity%20Report&Page=Environment%20Usage%20%26%20Assignments&PageIdentifier=dci6so08o36rkjab&BookmarkState=2a9eot3uvlit8881btmpso75bm&opti',
+                    GSEUCMEnvironmentsTotalNumber: 3,
+                    GSEUCMEnvironmentsZeroDemos: 0,
+                    TotalDemos: 5,
+                    FutureDemos: 0,
+                    DemosMonthtoDate: 0,
+                    DemosPriorMonth: 2,
+                    Demos2MonthsBack: 3,
+                    TotalPastDemos: 5,
+                    PercentwithZeroDemos: 0,
+                    DemosEnvMonth: 0.8
+                },
+                {
+                    id: '11',
+                    Group: 'PS',
+                    Team: 'PS',
+                    TeamLead: 'Aaron Cornfeld',
+                    GSEAccountOwner: '<a href="mailto:carlos.augusto@oracle.com">Carlos Augusto</a>',
+                    GSEReportLink: 'https://dss-obiee.oracleads.com/analytics/saw.dll?Dashboard&PortalPath=%2Fshared%2FGSE%20Public%2F_portal%2FDemo%20Activity%20Report&Page=Environment%20Usage%20%26%20Assignments&PageIdentifier=dci6so08o36rkjab&BookmarkState=hbhlktcin3g2tf82c2fk64bsaq&opti',
+                    GSEUCMEnvironmentsTotalNumber: 14,
+                    GSEUCMEnvironmentsZeroDemos: 9,
+                    TotalDemos: 5,
+                    FutureDemos: 1,
+                    DemosMonthtoDate: 0,
+                    DemosPriorMonth: 0,
+                    Demos2MonthsBack: 4,
+                    TotalPastDemos: 4,
+                    PercentwithZeroDemos: 64,
+                    DemosEnvMonth: 0.1
+                },
+                {
+                    id: '12',
+                    Group: 'CSM',
+                    Team: 'CSM',
+                    TeamLead: 'Misha Logvinov',
+                    GSEAccountOwner: '<a href="mailto:craig.lockwood@oracle.com">Craig Lockwood</a>',
+                    GSEReportLink: 'https://dss-obiee.oracleads.com/analytics/saw.dll?Dashboard&PortalPath=%2Fshared%2FGSE%20Public%2F_portal%2FDemo%20Activity%20Report&Page=Environment%20Usage%20%26%20Assignments&PageIdentifier=dci6so08o36rkjab&BookmarkState=sakn4n11gj52ns7leslj48lmtu&opti',
+                    GSEUCMEnvironmentsTotalNumber: 7,
+                    GSEUCMEnvironmentsZeroDemos: 2,
+                    TotalDemos: 18,
+                    FutureDemos: 3,
+                    DemosMonthtoDate: 1,
+                    DemosPriorMonth: 3,
+                    Demos2MonthsBack: 11,
+                    TotalPastDemos: 15,
+                    PercentwithZeroDemos: 29,
+                    DemosEnvMonth: 1.0
+                },
+                {
+                    id: '13',
+                    Group: 'Workshop/Hubs',
+                    Team: 'It includes Hub Instances as well as Hub Platform instances',
+                    TeamLead: 'Patrick Davies, Subhash Vanga, Jack Kingsley, Girish Venkat',
+                    GSEAccountOwner: '<a href="mailto:steven.nichols@oracle.com;subhash.vanga@oracle.com;jack.kingsley@oracle.com;v.girish@oracle.com">Steven Nichols, Subhash Vanga, Jack Kingsley, Girish Venkat</a>',
+                    GSEReportLink: 'https://dss-obiee.oracleads.com/analytics/saw.dll?Dashboard&PortalPath=%2Fshared%2FGSE%20Public%2F_portal%2FDemo%20Activity%20Report&Page=Environment%20Usage%20%26%20Assignments&PageIdentifier=dci6so08o36rkjab&BookmarkState=vbnoj8nrllbcn4asb1qq96kbvq&opti',
+                    GSEUCMEnvironmentsTotalNumber: 21,
+                    GSEUCMEnvironmentsZeroDemos: 9,
+                    TotalDemos: 25,
+                    FutureDemos: 2,
+                    DemosMonthtoDate: 2,
+                    DemosPriorMonth: 7,
+                    Demos2MonthsBack: 14,
+                    TotalPastDemos: 23,
+                    PercentwithZeroDemos: 43,
+                    DemosEnvMonth: 0.5
+                }
+            ];
+			//console.log("all data 2"+self.fetchdetails());
+            self.datasource = new oj.ArrayTableDataSource(envArray, {
                 idAttribute: 'id'
             });
             self.gsetotalnumber = 0;
@@ -300,16 +521,16 @@ self.subchart13 = {
             self.percentwithzero = 0;
             self.demosenvmonth = 0;
 
-            for (var i = 0; i < self.fetchdetails().length; i++) {
+            for (var i = 0; i < envArray.length; i++) {
 				
-                self.gsetotalnumber = self.fetchdetails()[i].GSEUCMEnvironmentsTotalNumber + self.gsetotalnumber;
-                self.gsezeronumber = self.fetchdetails()[i].GSEUCMEnvironmentsZeroDemos + self.gsezeronumber;
-                self.gsetotaldemo = self.fetchdetails()[i].TotalDemos + self.gsetotaldemo;
-                self.futuredemo = self.fetchdetails()[i].FutureDemos + self.futuredemo;
-                self.demosmonthtodate = self.fetchdetails()[i].DemosMonthtoDate + self.demosmonthtodate;
-                self.demospriormonth = self.fetchdetails()[i].DemosPriorMonth + self.demospriormonth;
-                self.demostwomonths = self.fetchdetails()[i].Demos2MonthsBack + self.demostwomonths;
-                self.totalpastdemo = self.fetchdetails()[i].TotalPastDemos + self.totalpastdemo;
+                self.gsetotalnumber = envArray[i].GSEUCMEnvironmentsTotalNumber + self.gsetotalnumber;
+                self.gsezeronumber = envArray[i].GSEUCMEnvironmentsZeroDemos + self.gsezeronumber;
+                self.gsetotaldemo = envArray[i].TotalDemos + self.gsetotaldemo;
+                self.futuredemo = envArray[i].FutureDemos + self.futuredemo;
+                self.demosmonthtodate = envArray[i].DemosMonthtoDate + self.demosmonthtodate;
+                self.demospriormonth = envArray[i].DemosPriorMonth + self.demospriormonth;
+                self.demostwomonths = envArray[i].Demos2MonthsBack + self.demostwomonths;
+                self.totalpastdemo = envArray[i].TotalPastDemos + self.totalpastdemo;
                 self.percentwithzero = Math.ceil(((self.gsezeronumber / self.gsetotalnumber) * 100));
                 self.demosenvmonth = ((self.totalpastdemo / self.gsetotalnumber) / 2.15).toFixed(1);
 
